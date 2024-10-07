4b3fc1737e48d22439f21d4b75a2186596ca1406
Rolando Ruiz Martinez <a00835733@tec.mx>
2024-10-07 16:25:54 -0600

    Avance Clases

---
+++ WinFormsApp1/Clases/Arco.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1.Clases
+{
+    public class Arco
+    {
+        // Nace IdPunto
+        public int IdPuntoOrigen { get; set; }
+        public int IdPuntoDestino { get; set; }
+        public double Distancia { get; set; }
+        public double Competitividad { get; set; }
+        public Arco()
+        {
+
+        }
+
+        public Arco(Arco arco)
+        {
+            IdPuntoOrigen = arco.IdPuntoOrigen;
+            IdPuntoDestino = arco.IdPuntoDestino;
+            Distancia = arco.Distancia;
+            Competitividad = arco.Competitividad;
+        }
+    }
+}
--- WinFormsApp1/Clases/Asignacion.cs
+++ WinFormsApp1/Clases/Asignacion.cs
@@ -8,5 +8,19 @@ namespace WinFormsApp1.Clases
 {
     public class Asignacion
     {
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
+        public int IdPunto { get; set; }
+        public bool Activo { get; set; }
+        public string Especie { get; set; }
+        public Asignacion()
+        public Asignacion(Asignacion asignacion)
+        {
+            Longitud = asignacion.Longitud;
+            Latitud = asignacion.Latitud;
+            Activo = asignacion.Activo;
+            Especie = asignacion.Especie;
+            IdPunto = asignacion.IdPunto;
+        }
     }
 }
