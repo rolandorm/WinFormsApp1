14b6f5125fb919c0e7ff8e6713affabd94de02e4
Rolando Ruiz Martinez <a00835733@tec.mx>
2024-10-11 23:48:30 -0600

    nuevos cambios excel nuevos

---
+++ WinFormsApp1/Algoritmos/CuadradoMedio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsApp1.Clases;
+
+namespace WinFormsApp1.Algoritmos
+{
+    public class CuadradoMedio
+    {
+        public List<int> AleatorioCuadradoMedio(int semilla)
+        {
+            List<int> listaAleatorios = new List<int>();
+            return [0];
+        }
+    }
+}
