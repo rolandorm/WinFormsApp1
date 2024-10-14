c8bb245d565b7e61f037bab386bc63d4ad3328be
Rolando Ruiz Martinez <a00835733@tec.mx>
2024-10-14 17:04:40 -0600

    Repositorio Aleatorios CM

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -30,19 +30,16 @@
         {
             button1 = new Button();
             label1 = new Label();
+            dataGridView1 = new DataGridView();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             label2 = new Label();
-            dataGridView1 = new DataGridView();
-            label3 = new Label();
-            textBox3 = new TextBox();
-            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(551, 86);
+            button1.Location = new Point(66, 344);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
@@ -55,14 +52,23 @@
             label1.AutoSize = true;
             label1.Location = new Point(78, 95);
             label1.Name = "label1";
-            label1.Size = new Size(104, 20);
+            label1.Size = new Size(58, 20);
             label1.TabIndex = 1;
-            label1.Text = "Puntos Totales";
+            label1.Text = "Semilla";
             label1.Click += label1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(219, 46);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1001, 461);
+            dataGridView1.TabIndex = 5;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(219, 92);
+            textBox1.Location = new Point(66, 118);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(125, 27);
             textBox1.TabIndex = 2;
@@ -70,66 +76,29 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(219, 184);
+            textBox2.Location = new Point(66, 211);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(125, 27);
-            textBox2.TabIndex = 3;
+            textBox2.TabIndex = 7;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(78, 187);
+            label2.Location = new Point(78, 188);
             label2.Name = "label2";
+            label2.RightToLeft = RightToLeft.Yes;
             label2.Size = new Size(63, 20);
-            label2.TabIndex = 4;
-            label2.Text = "Máximo";
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(447, 200);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(300, 188);
-            dataGridView1.TabIndex = 5;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(78, 282);
-            label3.Name = "label3";
-            label3.Size = new Size(60, 20);
-            label3.TabIndex = 7;
-            label3.Text = "Mínimo";
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(219, 279);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(125, 27);
-            textBox3.TabIndex = 6;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(78, 359);
-            button2.Name = "button2";
-            button2.Size = new Size(135, 29);
-            button2.TabIndex = 8;
-            button2.Text = "Descargar Excel";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
+            label2.TabIndex = 6;
+            label2.Text = "Max iter";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(label3);
-            Controls.Add(textBox3);
-            Controls.Add(dataGridView1);
-            Controls.Add(label2);
+            ClientSize = new Size(1246, 533);
             Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
             Controls.Add(textBox1);
             Controls.Add(label1);
             Controls.Add(button1);
@@ -144,12 +113,9 @@
 
         private Button button1;
         private Label label1;
+        private DataGridView dataGridView1;
         private TextBox textBox1;
         private TextBox textBox2;
         private Label label2;
-        private DataGridView dataGridView1;
-        private Label label3;
-        private TextBox textBox3;
-        private Button button2;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -12,45 +12,64 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text.Equals("") || textBox2.Text.Equals("") || textBox3.Text.Equals(""))
+            if (textBox1.Text.Equals("") || textBox2.Text.Equals(""))
             {
                 MessageBox.Show("Los números tienen que ser MAYOR que cero, NO VACÍOS");
                 return;
             }
 
-            int puntosTotales = Convert.ToInt32(textBox1.Text);
-            int maximo = Convert.ToInt32(textBox2.Text);
-            int minimo = Convert.ToInt32((textBox3.Text));
+            int Semilla = Convert.ToInt32(textBox1.Text);
+            int Maxiter = Convert.ToInt32(textBox2.Text);
 
-            if (puntosTotales < 0 || maximo < 0 || minimo < 0)
-            {
-
-            }
-
-            GeneradorAleatorios generador = new GeneradorAleatorios();
-            List<Asignacion> listaSalida = generador.CrearListaOrigen(puntosTotales, minimo, maximo);
+            ClassCM generador = new ClassCM();
+            List<List<int>> listaSalida = generador.AleatorioCuadradoMedio(Semilla,Maxiter);
             llenarGrid(listaSalida);
         }
 
-        public void llenarGrid(List<Asignacion> lista)
+        private void llenarGrid(List<List<int>> listaDeListas)
         {
-            string numeroColumna1 = "1";
-            string numeroColumna2 = "2";
-            string numeroColumna3 = "3";
-
+            // Limpiar el DataGridView antes de llenarlo
+            dataGridView1.Rows.Clear();
             dataGridView1.Columns.Clear();
-            dataGridView1.Columns.Add(numeroColumna1, "Id");
-            dataGridView1.Columns.Add(numeroColumna2, "Latitud");
-            dataGridView1.Columns.Add(numeroColumna3, "Longitud");
 
-            for (int i = 0; i < lista.Count; i++)
+            // Establecer un número de columnas dinámico basado en el número de listas
+            int numeroDeColumnas = listaDeListas.Count;
+
+            dataGridView1.Columns.Add("Aleatorio","Aleatorio");
+            dataGridView1.Columns.Add("Cuadrado", "Cuadrado");
+            dataGridView1.Columns.Add("Centros", "Centros");
+            dataGridView1.Columns.Add("Izquierdo", "Izquierdo");
+            dataGridView1.Columns.Add("Derecho", "Derecho");
+
+
+
+
+
+            // Determinar el número máximo de filas basado en la lista más larga
+            int numeroDeFilas = listaDeListas.Max(l => l.Count);
+
+            // Añadir filas al DataGridView de forma transpuesta
+            for (int i = 0; i < numeroDeFilas; i++)
             {
-                dataGridView1.Rows.Add();
-                dataGridView1.Rows[i].Cells[Int32.Parse(numeroColumna1) - 1].Value = lista[i].IdPunto.ToString();
-                dataGridView1.Rows[i].Cells[Int32.Parse(numeroColumna2) - 1].Value = lista[i].Latitud.ToString();
-                dataGridView1.Rows[i].Cells[Int32.Parse(numeroColumna3) - 1].Value = lista[i].Longitud.ToString();
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(dataGridView1);
+
+                // Rellenar cada fila con los valores correspondientes
+                for (int j = 0; j < numeroDeColumnas; j++)
+                {
+                    if (i < listaDeListas[j].Count) // Verifica si hay un valor en la posición actual
+                    {
+                        row.Cells[j].Value = listaDeListas[j][i];
+                    }
+                    else
+                    {
+                        row.Cells[j].Value = ""; // Dejar la celda vacía si no hay más elementos
+                    }
+                }
+                dataGridView1.Rows.Add(row);
             }
         }
+
 
         public void DescargaExcel(DataGridView data)
         {
